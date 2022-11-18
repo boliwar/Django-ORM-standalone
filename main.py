7933c3f9b6d44f8886c6484dce3a6d7cedd40a48
boliwar <boliwar@gmail.com>
2022-11-18 10:01:21 +0200

    Фильтрация данных

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ from datacenter.models import Passcard  # noqa: E402
 if __name__ == '__main__':
     # Программируем здесь
     print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
-    passcards = Passcard.objects.all()
-
-    active_passcards =[passcard for passcard in passcards if passcard.is_active]
+    
+    active_passcards = Passcard.objects.filter(is_active=True)
     print(f"Активных пропусков {len(active_passcards)}")
