ceea40356116ad20e0d6b9dbe30c16dcba8b1188
boliwar <boliwar@gmail.com>
2022-11-18 10:47:14 +0200

    Для каждого посетителя

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 
 import django
+import settings
+from django.utils.timezone import localtime as dj_localtime, now as dj_now
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 django.setup()
@@ -12,4 +14,12 @@ if __name__ == '__main__':
     print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
 
     unclosed_visits = Visit.objects.filter(leaved_at__isnull=True)
-    print(unclosed_visits)
+
+    for unclosed_visit in unclosed_visits:
+        entered_msk = dj_localtime(unclosed_visit.entered_at)
+        print(f'Зашёл в хранилище, время по Москве: \n{entered_msk}')
+        print()
+        time_in =  dj_now() - entered_msk
+        print(f'Находится в хранилище:\n{time_in}')
+        print('---')
+    
