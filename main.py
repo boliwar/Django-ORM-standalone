0b3fc7138f6d7794958877b7aaeb9536f9f50614
boliwar <boliwar@gmail.com>
2022-11-18 09:53:34 +0200

    Вы получите имя владельца, код, время создания и статус активности любого из пропусков.

--- main.py
+++ main.py
@@ -11,4 +11,10 @@ if __name__ == '__main__':
     # Программируем здесь
     print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
     passcards = Passcard.objects.all()
-    print(passcards)
+
+    owner_name = passcards[0].owner_name
+    passcode = passcards[0].passcode
+    created_at = passcards[0].created_at
+    is_active = passcards[0].is_active
+
+    print(owner_name, passcode, created_at, is_active)
