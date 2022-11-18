1b5db69adffa950e04c002d0657232cdea7bf019
boliwar <boliwar@gmail.com>
2022-11-18 10:26:15 +0200

    Список незакрытых визитов

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 django.setup()
 
-from datacenter.models import Passcard  # noqa: E402
+from datacenter.models import Passcard, Visit  # noqa: E402
 
 if __name__ == '__main__':
     # Программируем здесь
     print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
-    
-    active_passcards = Passcard.objects.filter(is_active=True)
-    print(f"Активных пропусков {len(active_passcards)}")
+
+    unclosed_visits = Visit.objects.filter(leaved_at__isnull=True)
+    print(unclosed_visits)
