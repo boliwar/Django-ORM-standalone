9943854074dbbd3b0ce5a72a1e4c9292b33667ee
boliwar <boliwar@gmail.com>
2022-11-18 09:59:05 +0200

    Активных пропусков

--- main.py
+++ main.py
@@ -12,9 +12,5 @@ if __name__ == '__main__':
     print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
     passcards = Passcard.objects.all()
 
-    owner_name = passcards[0].owner_name
-    passcode = passcards[0].passcode
-    created_at = passcards[0].created_at
-    is_active = passcards[0].is_active
-
-    print(owner_name, passcode, created_at, is_active)
+    active_passcards =[passcard for passcard in passcards if passcard.is_active]
+    print(f"Активных пропусков {len(active_passcards)}")
